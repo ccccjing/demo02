6784ee4d032205a95dcedffc25b267d91cde2cec
PIERCE <2391166541@qq.com>
2022-09-26 13:16:47 +0800

    feat: 柱状图

--- src/router/modules/charts.js
+++ src/router/modules/charts.js
@@ -3,15 +3,21 @@ import Layout from '@/layout'
 const codeRouter = {
   path: '/charts',
   component: Layout,
-  redirect: '/charts/pictorial-bar',
+  redirect: '/charts/bar-charts',
   name: 'Charts',
-  meta: { title: '视图模型', icon: 'el-icon-postcard', roles: 'admin' },
+  meta: { title: '视图模型', icon: 'el-icon-s-data', roles: 'admin' },
   children: [
     {
       path: 'pictorial-bar',
       component: () => import('@/views/charts/pictorial-bar'),
       name: 'PictorialBar',
       meta: { title: '驼峰' }
+    },
+    {
+      path: 'bar-charts',
+      component: () => import('@/views/charts/bar-charts'),
+      name: 'BarCharts',
+      meta: { title: '柱状图' }
     },
   ]
 }
---
+++ src/views/charts/bar-charts.vue
@@ -0,0 +1,51 @@
+<!--
+ * @description: description
+ * @fileName: bar-charts.vue
+ * @author: pierce
+ * @lastDate: 2022-09-26 13:11:58
+!-->
+
+<template>
+  <div>
+    <div id="main"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+  name: "PictorialBar",
+  mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    let myChart = echarts.init(document.getElementById("main"));
+
+    let option = {
+      tooltip: {},
+      legend: {},
+      xAxis: {
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      },
+      yAxis: {},
+      series: [
+        {
+          name: "Sale",
+          type: "bar",
+          data: [5, 20, 36, 10, 10, 20, 4],
+          colorBy: "data",
+        },
+      ],
+    };
+
+    // 绘制图表
+    myChart.setOption(option);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#main {
+  width: 100%;
+  height: 600px;
+}
+</style>
